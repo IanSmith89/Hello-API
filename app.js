ccc3c120dda66650c210e577d8a0d933a5045bdd
Jordan Majd <jordan.majd@galvanize.com>
2015-12-09 15:46:49 -0700

    Revert "completed the exercise"

--- app.js
+++ app.js
@@ -1,14 +1,7 @@
 'use strict';
 
 //TODO implement the jQuery ajax call here and console log the data
-function getData() {
-  $.get("http://localhost:8000/api", function(data) {
-    // alert('Data loaded:' + data);
-    console.log(data);
-  });
-}
 
-getData();
 //See https://api.jquery.com/jquery.get/ for an example of the get function
 
 //Alternatively
